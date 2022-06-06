9317c23913b9225420af9c1bf31d82016ef41b55
Akash <bagakash11@gmail.com>
2022-06-06 21:49:56 +0530

    add to the top button added about, products section, updated footer

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function myFunction() {
-  window.location.href = "index.html";
-}
-const element = document.getElementById("products");
-element.addEventListener("click", myFunction);
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
